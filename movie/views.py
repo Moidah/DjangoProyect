c9b4630b3aa11597911223f8f1a3b6106d65ec50
Moisés Arrieta <moisesdavidarh@gmail.com>
2023-08-03 11:33:55 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>welcome to home page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Moises Arrieta'})
