1bebbad2b62819f5348a65c409866f0515b27e96
Moisés Arrieta <moisesdavidarh@gmail.com>
2023-08-31 14:27:36 -0500

    agregacion de las movies

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
+from .models import Movie
+
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else: 
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 
 def about(request):
-    return HttpResponse('<h1>welcome to About page</h1>')
-    #return render(request, 'home.html')
-    #return render(request, 'home.html', {'name':'Moises Arrieta'})
+    return render(request, 'about.html')
+
+    
+    
+    
